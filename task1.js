a31a3664ec1a2f45764f7f712bef015d7c01305f
farhan <farhanrizqi008@gmail.com>
2023-07-07 00:52:15 +0700

    initiate

---
+++ task1.js
@@ -0,0 +1,52 @@
+// ! nomor 1
+const biodata = {
+  name: "Farhan Rizqi Aminullah",
+  age: 23,
+  hobbies: ["mendengarkan musik", "olahraga", "bermain game"],
+  IsMarried: false,
+  schoolList: [
+    {
+      name: "SDN Ibu Jenab 1",
+      yearIn: 2006,
+      yearOut: 2012,
+      major: null,
+    },
+    {
+      name: "SMPN 4 Cianjur",
+      yearIn: 2012,
+      yearOut: 2015,
+      major: null,
+    },
+    {
+      name: "SMKN 1 Cianjur",
+      yearIn: 2015,
+      yearOut: 2018,
+      major: "Rekayasa Perangkat Lunak",
+    },
+    {
+      name: "Universitas Pendidikan Indonesia",
+      yearIn: 2018,
+      yearOut: 2023,
+      major: "Pendidikan Bahasa Prancis",
+    },
+  ],
+  skills: [
+    {
+      skillName: "Programming",
+      level: "beginner",
+    },
+    {
+      skillName: "Microsoft Office",
+      level: "beginner",
+    },
+    {
+      skillName: "French",
+      level: "beginner",
+    },
+    {
+      skillName: "English",
+      level: "advanced",
+    },
+  ],
+  interestInCoding: true,
+};
